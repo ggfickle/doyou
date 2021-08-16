53439db0f9c18f5c78e0b9f0542e893d9b5d950c
Hoongfei <841192493@qq.vom>
2021-08-17 00:20:47 +0800

    add file

---
+++ blog-vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <bar/>
+    <router-view/>
+    <bottom/>
+  </div>
+</template>
+
+<script>
+  import bar from '@/components/bar'
+  import bottom from '@/components/bottom'
+
+  export default {
+    components: {bar, bottom},
+    name: 'App'
+  }
+</script>
+
+<style>
+  #app {
+
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+    text-align: center;
+    margin-top: 55px;
+    padding-left: 10%;
+    padding-right: 10%;
+    background-color: rgba(242, 245, 252, 0.3);
+
+  }
+</style>
